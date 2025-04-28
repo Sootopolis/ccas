6f9745b1cedd5d51eaf67c6eedd5d29294b900b8
wallace <yuzhuo.w@gmail.com>
2025-04-29 00:00:28 +0100

    sql

--- src/main/scala/ccas/analysis/tables/general/PlayerSnapshot.scala
+++ src/main/scala/ccas/analysis/tables/general/PlayerSnapshot.scala
@@ -2,15 +2,13 @@ package ccas.analysis.tables.general
 
 import ccas.api.misc.enums.{PlayerStatusCategory, Title}
 import ccas.api.misc.subtypes.{PlayerId, Username}
-import ccas.utils.sql.CcasPostgresContext
 import io.getquill.*
-import io.getquill.context.qzio.ZioJdbcContext
 import io.getquill.extras.InstantOps
-import zio.ZIO
+import io.getquill.jdbczio.Quill
+import zio.{Tag, ZIO}
 
 import java.sql.SQLException
 import java.time.Instant
-import javax.sql.DataSource
 
 case class PlayerSnapshot(
   playerId: PlayerId,
@@ -21,58 +19,59 @@ case class PlayerSnapshot(
 )
 
 object PlayerSnapshot {
-
-  import CcasPostgresContext.*
-
   inline given InsertMeta[PlayerSnapshot] = insertMeta()
 
   inline given UpdateMeta[PlayerSnapshot] = updateMeta(_.playerId, _.since)
 
-  inline def selectAll: CcasPostgresZIO[List[PlayerSnapshot]] = ZIO.serviceWithZIO[CcasPostgresContext] { context =>
-    import context.*
-    run(query[PlayerSnapshot])
+  inline def selectAll: ZIO[Quill.Postgres[SnakeCase], SQLException, List[PlayerSnapshot]] = {
+    ZIO.serviceWithZIO[Quill.Postgres[SnakeCase]] { quill =>
+      import io.getquill.*
+      import quill.*
+      run(query[PlayerSnapshot])
+    }
   }
 
-  inline def selectAllLatest: CcasPostgresZIO[List[PlayerSnapshot]] = {
-    ZIO.serviceWithZIO[CcasPostgresContext] { context =>
-      import context.*
+  inline def selectAllLatest: ZIO[Quill.Postgres[SnakeCase], SQLException, List[PlayerSnapshot]] = {
+    ZIO.serviceWithZIO[Quill.Postgres[SnakeCase]] { quill =>
+      import io.getquill.*
+      import quill.*
       run {
         query[PlayerSnapshot]
           .join { query[PlayerSnapshot].groupByMap(_.playerId)(row => row.playerId -> max(row.since)) }
           .on { case (row, (playerId, since)) => row.playerId == playerId && row.since == since }
           .map(_._1)
       }
-
     }
   }
 
-  inline def selectAllForId(playerId: PlayerId): CcasPostgresZIO[List[PlayerSnapshot]] = {
-    ZIO.serviceWithZIO[CcasPostgresContext] { context =>
-      import context.*
+  inline def selectAllForId(playerId: PlayerId): ZIO[Quill.Postgres[SnakeCase], SQLException, List[PlayerSnapshot]] = {
+    ZIO.serviceWithZIO[Quill.Postgres[SnakeCase]] { quill =>
+      import io.getquill.*
+      import quill.*
       run {
         query[PlayerSnapshot]
           .filter(_.playerId == lift(playerId))
       }
-
     }
   }
 
-  inline def selectLatestForId(playerId: PlayerId): CcasPostgresZIO[Option[PlayerSnapshot]] = {
-    ZIO.serviceWithZIO[CcasPostgresContext] { context =>
-      import context.*
+  inline def selectLatestForId(playerId: PlayerId): ZIO[Quill.Postgres[SnakeCase], SQLException, Option[PlayerSnapshot]] = {
+    ZIO.serviceWithZIO[Quill.Postgres[SnakeCase]] { quill =>
+      import io.getquill.*
+      import quill.*
       run {
         query[PlayerSnapshot]
           .filter(_.playerId == lift(playerId))
           .sortBy(_.since)(Ord.desc)
           .value
       }
-
     }
   }
 
-  inline def selectAllSince(since: Instant): CcasPostgresZIO[List[PlayerSnapshot]] = {
-    ZIO.serviceWithZIO[CcasPostgresContext] { context =>
-      import context.*
+  inline def selectAllSince(since: Instant): ZIO[Quill.Postgres[SnakeCase], SQLException, List[PlayerSnapshot]] = {
+    ZIO.serviceWithZIO[Quill.Postgres[SnakeCase]] { quill =>
+      import io.getquill.*
+      import quill.*
       run {
         val justBefore = query[PlayerSnapshot]
           .join(
@@ -82,30 +81,29 @@ object PlayerSnapshot {
           )
           .on { case (row, (playerId, since)) => row.playerId == playerId && row.since == since }
           .map(_._1)
-
         val after = query[PlayerSnapshot]
           .filter(_.since >= lift(since))
         justBefore.union(after)
       }
-
     }
   }
 
-  inline def insert(item: PlayerSnapshot): CcasPostgresZIO[PlayerSnapshot] = {
-    ZIO.serviceWithZIO[CcasPostgresContext] { context =>
-      import context.*
+  inline def insert(item: PlayerSnapshot): ZIO[Quill.Postgres[SnakeCase], SQLException, PlayerSnapshot] = {
+    ZIO.serviceWithZIO[Quill.Postgres[SnakeCase]] { quill =>
+      import io.getquill.*
+      import quill.*
       run {
         query[PlayerSnapshot]
           .insertValue(lift(item))
           .returning(x => x)
       }
-
     }
   }
 
-  inline def update(item: PlayerSnapshot): CcasPostgresZIO[PlayerSnapshot] = {
-    ZIO.serviceWithZIO[CcasPostgresContext] { context =>
-      import context.*
+  inline def update(item: PlayerSnapshot): ZIO[Quill.Postgres[SnakeCase], SQLException, PlayerSnapshot] = {
+    ZIO.serviceWithZIO[Quill.Postgres[SnakeCase]] { quill =>
+      import io.getquill.*
+      import quill.*
       run {
         query[PlayerSnapshot]
           .filter(_.playerId == lift(item.playerId))
@@ -113,13 +111,13 @@ object PlayerSnapshot {
           .updateValue(lift(item))
           .returning(x => x)
       }
-
     }
   }
 
-  inline def delete(key: PlayerId, since: Instant): CcasPostgresZIO[PlayerSnapshot] = {
-    ZIO.serviceWithZIO[CcasPostgresContext] { context =>
-      import context.*
+  inline def delete(key: PlayerId, since: Instant): ZIO[Quill.Postgres[SnakeCase], SQLException, PlayerSnapshot] = {
+    ZIO.serviceWithZIO[Quill.Postgres[SnakeCase]] { quill =>
+      import io.getquill.*
+      import quill.*
       run {
         query[PlayerSnapshot]
           .filter(_.playerId == lift(key))
